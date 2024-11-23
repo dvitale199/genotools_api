b1f065ef771cb992a7be0f59fc5647518e4d28a0
dvitale199 <d.vitale199@gmail.com>
2024-11-23 00:00:21 +0000

    updated cloud_run_deploy.sh

--- scripts/cloud_run_deploy.sh
+++ scripts/cloud_run_deploy.sh
@@ -2,8 +2,16 @@ docker buildx build --platform linux/amd64 -t europe-west4-docker.pkg.dev/gp2-re
 
 docker push europe-west4-docker.pkg.dev/gp2-release-terra/genotools/genotools-api
 
+# gcloud run deploy genotools-api \
+#   --image europe-west4-docker.pkg.dev/gp2-release-terra/genotools/genotools-api \
+#   --platform managed \
+#   --region europe-west4 \
+#   --service-account genotools-server@gp2-release-terra.iam.gserviceaccount.com
+
 gcloud run deploy genotools-api \
   --image europe-west4-docker.pkg.dev/gp2-release-terra/genotools/genotools-api \
   --platform managed \
   --region europe-west4 \
-  --service-account genotools-server@gp2-release-terra.iam.gserviceaccount.com
+  --service-account genotools-server@gp2-release-terra.iam.gserviceaccount.com \
+  --set-env-vars API_KEY="your_api_key_value",API_KEY_NAME="X-API-KEY" \
+  --allow-unauthenticated
