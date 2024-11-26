c878b8b282cc83fb599327660ad923cc42bdbf88
dvitale199 <d.vitale199@gmail.com>
2024-11-26 22:31:59 +0000

    updated api key to use google secret manager method

--- scripts/cloud_run_deploy.sh
+++ scripts/cloud_run_deploy.sh
@@ -15,3 +15,18 @@ gcloud run deploy genotools-api \
   --service-account genotools-server@gp2-release-terra.iam.gserviceaccount.com \
   --set-env-vars API_KEY="your_api_key_value",API_KEY_NAME="X-API-KEY" \
   --allow-unauthenticated
+
+
+
+
+
+
+# setup api key
+APIKEY=<APIKEY>
+echo -n $APIKEY | gcloud secrets create genotools-api-key --data-file=-
+echo -n $APIKEY | gcloud secrets versions add genotools-api-key --data-file=- 
+
+# give service account access to key
+gcloud secrets add-iam-policy-binding genotools-api-key \
+    --member="serviceAccount:genotools-server@gp2-release-terra.iam.gserviceaccount.com" \
+    --role="roles/secretmanager.secretAccessor"
